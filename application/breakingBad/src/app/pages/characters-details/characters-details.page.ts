4592ccd55e7b163e62478e0d084fccf7897203f8
BrenKenna <brend.k.bk@gmail.com>
2022-05-13 12:16:51 +0000

    characters basic

--- application/breakingBad/src/app/pages/characters-details/characters-details.page.ts
+++ application/breakingBad/src/app/pages/characters-details/characters-details.page.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 
+// Support loading page with input from another page
+import { ActivatedRoute } from '@angular/router';
+import { QueryAPIService } from '../../services/queryAPI/query-api.service';
+import { FavouriteService } from '../../services/favourites/favourites.service';
+
 @Component({
   selector: 'app-characters-details',
   templateUrl: './characters-details.page.html',
-  styleUrls: ['./characters-details.page.scss'],
+  styleUrls: ['./characters-details.page.scss', '../episodes-details/episodes-details.page.scss'],
 })
 export class CharactersDetailsPage implements OnInit {
 
-  constructor() { }
+  // Attributes
+  character: any;
+  characterID = null;
+  isFavourite = false;
+  _STORAGE_CONTEXT = "Characters";
+  tabPath: string;
+  apiPath: string;
 
+  /**
+   * 
+   * @param activatedRoute 
+   * @param queryAPI 
+   * @param favServ 
+   */
+  constructor(private activatedRoute: ActivatedRoute, private queryAPI : QueryAPIService, private favServ: FavouriteService) {
+    this.tabPath = queryAPI.getTabPath("Characters");
+    this.apiPath = queryAPI.getAPIPath("Characters");
+  };
+ 
+
+  /**
+   * 
+   */
   ngOnInit() {
+    this.characterID = this.activatedRoute.snapshot.paramMap.get('id');
+    this.queryAPI.getCharacter(`${this.characterID}`).subscribe(res => {
+      this.character = res[0];
+    });
+
+    this.favServ.isFavourited(this.characterID, this._STORAGE_CONTEXT).then(isFav => {
+      this.isFavourite = isFav;
+    });
   }
 
+  /**
+   * 
+   */
+   favouriteCharacter() {
+    this.favServ.favourite(this.characterID, this._STORAGE_CONTEXT).then(() => {
+      this.isFavourite = true;
+    });
+  }
+
+
+  /**
+   * 
+   */
+  unfavouriteCharacter() {
+    this.favServ.unfavourite(this.characterID, this._STORAGE_CONTEXT).then(() => {
+      this.isFavourite = false;
+    });
+  }
 }
--- application/breakingBad/src/app/pages/characters/characters.page.ts
+++ application/breakingBad/src/app/pages/characters/characters.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+// Import API service
+import { QueryAPIService } from '../../services/queryAPI/query-api.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-characters',
@@ -7,9 +14,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersPage implements OnInit {
 
-  constructor() { }
+  // Holds results from consuming API
+  characters: Observable<any>;
+  tabPath: string;
+  apiPath: string;
 
+
+  /**
+   * Construct Character pages with HTTP client and API serivce to fetch results, 
+   * so that a specific page can be inspected via the routing module
+   * 
+   * @param router - Router
+   * @param queryAPI - QueryAPIService, Custom
+   */
+  constructor(private router: Router, private queryAPI: QueryAPIService) {
+    this.tabPath = queryAPI.getTabPath("Characters");
+    this.apiPath = queryAPI.getAPIPath("Characters");
+  }
+
+
+  /**
+   * Lazy load promise
+   */
   ngOnInit() {
+
+    // Fetch api data and sanity check
+    this.characters = this.queryAPI.fetchCharacters();
+
+    // Sanity check data is returned
+    this.characters.subscribe(data => {
+      console.log(
+        `Data from "${this.apiPath}" contains "${data.length}" values and a value will be routed to "${this.tabPath}"`
+      );
+      console.dir(data[0], {depth: null});
+    });
+  }
+
+
+  /**
+   * Navigate to epsiode page
+   * 
+   * @param episode 
+   */
+  openDetails(character) {
+    let characterID = character.char_id;
+    console.log(`Attempting to navigate to: ${this.tabPath}/${characterID}`);
+    this.router.navigateByUrl(`${this.tabPath}/${characterID}`);
   }
 
 }
