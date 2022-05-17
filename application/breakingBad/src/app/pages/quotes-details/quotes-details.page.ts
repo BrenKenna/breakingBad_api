fc4be4e88c9b324065376e69e6e9ad6503a1e379
BrenKenna <brend.k.bk@gmail.com>
2022-05-17 08:29:38 +0000

    initial quote pages

--- application/breakingBad/src/app/pages/quotes-details/quotes-details.page.ts
+++ application/breakingBad/src/app/pages/quotes-details/quotes-details.page.ts
@@ -1,4 +1,12 @@
+// 
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+// Import API service
+import { QueryAPIService } from '../../services/queryAPI/query-api.service';
+import { FavouriteService } from '../../services/favourites/favourites.service';
+
 
 @Component({
   selector: 'app-quotes-details',
@@ -7,9 +15,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesDetailsPage implements OnInit {
 
-  constructor() { }
+  // Attributes
+  quote: any;
+  quoteID = null;
+  isFavourite = false;
+  _STORAGE_CONTEXT = "Quotes";
+  tabPath: string;
+  apiPath: string;
 
+  /**
+   * 
+   * @param activatedRoute 
+   * @param queryAPI 
+   * @param favServ 
+   */
+  constructor(private activatedRoute: ActivatedRoute, private queryAPI : QueryAPIService, private favServ: FavouriteService) {
+    this.tabPath = queryAPI.getTabPath("Quotes");
+    this.apiPath = queryAPI.getAPIPath("Quotes");
+  };
+ 
+
+  /**
+   * 
+   */
   ngOnInit() {
+    this.quoteID = this.activatedRoute.snapshot.paramMap.get('id');
+    this.queryAPI.getQuote(`${this.quoteID}`).subscribe(res => {
+      this.quote = res[0];
+    });
+
+    this.favServ.isFavourited(this.quoteID, this._STORAGE_CONTEXT).then(isFav => {
+      this.isFavourite = isFav;
+    });
+  }
+
+  /**
+   * 
+   */
+   favouriteCharacter() {
+    this.favServ.favourite(this.quoteID, this._STORAGE_CONTEXT).then(() => {
+      this.isFavourite = true;
+    });
+  }
+
+
+  /**
+   * 
+   */
+  unfavouriteCharacter() {
+    this.favServ.unfavourite(this.quoteID, this._STORAGE_CONTEXT).then(() => {
+      this.isFavourite = false;
+    });
   }
 
 }
--- application/breakingBad/src/app/pages/quotes/quotes.page.ts
+++ application/breakingBad/src/app/pages/quotes/quotes.page.ts
@@ -1,4 +1,13 @@
+// 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+// Import API service
+import { QueryAPIService } from '../../services/queryAPI/query-api.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-quotes',
@@ -7,9 +16,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesPage implements OnInit {
 
-  constructor() { }
+  // Holds results from consuming API
+  quotes: Observable<any>;
+  tabPath: string;
+  apiPath: string;
 
-  ngOnInit() {
+
+  /**
+   * Construct Character pages with HTTP client and API serivce to fetch results, 
+   * so that a specific page can be inspected via the routing module
+   * 
+   * @param router - Router
+   * @param queryAPI - QueryAPIService, Custom
+   */
+  constructor(private router: Router, private queryAPI: QueryAPIService) {
+    this.tabPath = queryAPI.getTabPath("Quotes");
+    this.apiPath = queryAPI.getAPIPath("Quotes");
   }
 
+  /**
+   * Lazy load promise
+   */
+   ngOnInit() {
+
+    // Fetch api data and sanity check
+    this.quotes = this.queryAPI.fetchQuotes();
+
+    // Sanity check data is returned
+    this.quotes.subscribe(data => {
+      console.log(
+        `Data from "${this.apiPath}" contains "${data.length}" values and a value will be routed to "${this.tabPath}"`
+      );
+      console.dir(data[0], {depth: null});
+    });
+  }
+
+  /**
+   * Navigate to epsiode page
+   * 
+   * @param episode 
+   */
+   openDetails(quote) {
+    let quoteID = quote.quote_id;
+    console.log(`Attempting to navigate to: ${this.tabPath}/${quoteID}`);
+    this.router.navigateByUrl(`${this.tabPath}/${quoteID}`);
+  }
 }
